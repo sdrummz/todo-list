fedb4cf1bab6ac8aa88fc6f5ec284efaa6c3e234
szglabs <sdrummond95@gmail.com>
2021-05-10 23:11:08 -0400

    task-actions with tooltips

--- src/app.js
+++ src/app.js
@@ -3,3 +3,7 @@ import { projectFactory } from './projects.js';
 document.querySelector('.navbar-toggler').addEventListener('click', function()  {
     document.querySelector('.left-nav').classList.toggle('reveal');
 })
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
