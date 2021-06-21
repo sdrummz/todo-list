ecb261607d36e727da7be9fb0b8fbb3ffd57c171
szglabs <sdrummond95@gmail.com>
2021-06-20 23:37:04 -0400

    add task modal adds to task storage

--- src/tasks.js
+++ src/tasks.js
@@ -2,36 +2,52 @@ import * as storage from './storage';
 import * as view from './view';
 import { newID } from './projects';
 
-
 const taskFactory = (title, description, project, dueDate, priority, projectID) => {
-    const taskID = newID();
-    return {title, description, project, dueDate, priority, taskID, projectID};
-}
+  const taskID = newID();
+  return { title, description, project, dueDate, priority, taskID, projectID };
+};
 
-// add task modal submit
+const addTaskModal = new bootstrap.Modal(document.getElementById('addTaskModal'));
 
-const taskTitle = document.getElementById('taskTitle');
-const taskDescription = document.getElementById('taskDescription');
-const taskProject = document.getElementById('taskProject');
-const taskDueDate = document.getElementById('taskDueDate');
-const taskPriority = document.getElementById('taskPriority');
-const addTaskModal = new bootstrap.Modal(document.getElementById('addTaskModal'))
-const taskForm = document.getElementById('taskForm');
-
+// saves add task modal submit for new task, logic for submit btn event listener
 const saveNewTask = () => {
-    taskForm.classList.add('was-validated');
-    if  (taskTitle.checkValidity() && taskProject.checkValidity() && taskDueDate.checkValidity() && taskPriority.checkValidity()) {
-        storage.taskStorage.push(taskFactory(taskTitle.value, taskDescription.value, taskProject.value, taskDueDate.value, taskPriority.value))
-    }
-    console.log(storage.taskStorage);
-}
+  // dom elements
+  const taskTitle = document.getElementById('taskTitle');
+  const taskDescription = document.getElementById('taskDescription');
+  const taskProject = document.getElementById('taskProject');
+  const taskDueDate = document.getElementById('taskDueDate');
+  const taskPriority = document.getElementById('taskPriority');
+  const taskForm = document.getElementById('taskForm');
 
+  //   submit logic
+  taskForm.classList.add('was-validated');
+  if (taskTitle.checkValidity() && taskProject.checkValidity() && taskDueDate.checkValidity() && taskPriority.checkValidity()) {
+    storage.taskStorage.push(
+      taskFactory(
+        taskTitle.value,
+        taskDescription.value,
+        taskProject.value,
+        taskDueDate.value,
+        taskPriority.value,
+        retrieveProjectID(taskProject.value)
+      )
+    );
+    addTaskModal.toggle();
+    taskForm.classList.remove('was-validated');
+    taskTitle.value = null;
+    taskDescription.value = null;
+    taskProject.value = null;
+    taskDueDate.value = null;
+    taskPriority.value = null;
+  }
+};
+
+// returns the project ID of the project selected, so tasks can be later grouped by projectID
 const retrieveProjectID = (taskProject) => {
-    
-}
+  return storage.projectStorage.find((e) => e.name === taskProject).projectID;
+};
 
 const saveTaskBtn = document.querySelector('.saveTaskBtn');
 saveTaskBtn.addEventListener('click', saveNewTask);
 
-
-export { taskFactory }
+export { taskFactory };
--- src/view.js
+++ src/view.js
@@ -2,46 +2,46 @@ import { projectStorage } from './storage';
 
 // Switch arrow function switches arrow direction on projects dropdown
 const switchArrow = () => {
-    const arrow = document.getElementById('arrow');
-    if (arrow.getAttribute('src') == "images/arrow-down.svg") {
-        arrow.src = "images/arrow-up.svg";
-    } else if (arrow.getAttribute('src') == "images/arrow-up.svg") {
-        arrow.src = "images/arrow-down.svg";
-    }
-}
+  const arrow = document.getElementById('arrow');
+  if (arrow.getAttribute('src') == 'images/arrow-down.svg') {
+    arrow.src = 'images/arrow-up.svg';
+  } else if (arrow.getAttribute('src') == 'images/arrow-up.svg') {
+    arrow.src = 'images/arrow-down.svg';
+  }
+};
 
 // Switching selected menu categories (default it today)
 const taskGroupTitle = (taskGroup) => {
-    let title = document.querySelector('.task-group-title');
-    title.textContent = taskGroup;
-}
-
+  let title = document.querySelector('.task-group-title');
+  title.textContent = taskGroup;
+};
 
 // Adding new projects and tasks to UI
 
 //  update/add projects list
 const updateProjectList = () => {
-    // parent node/list
-    const projectList = document.querySelector('.project-list');
+  // parent node/list
+  const projectList = document.querySelector('.project-list');
 
-    // iterate over project items to delete
-    const projectItems = document.querySelectorAll('.project-title');
-    projectItems.forEach(function(item) {
-        item.remove();
-    })
-    // for each project object, add to view
-    projectStorage.forEach(function(el) {
-        projectList.children[projectList.childElementCount - 1].insertAdjacentHTML("beforebegin", `<li class="project-title" data-project-ID=${el.projectID}>${el.name}</li>`);
-    })
-    // add event listeners to the project btns
-    let projectBtns = document.querySelectorAll('.project-title');
-    projectBtns.forEach(el => {
-        el.addEventListener('click', function() {
-            taskGroupTitle(`${el.textContent}`);
-        })
-    })
-}
-
-
+  // iterate over project items to delete
+  const projectItems = document.querySelectorAll('.project-title');
+  projectItems.forEach(function (item) {
+    item.remove();
+  });
+  // for each project object, add to view
+  projectStorage.forEach(function (el) {
+    projectList.children[projectList.childElementCount - 1].insertAdjacentHTML(
+      'beforebegin',
+      `<li class="project-title" data-project-ID=${el.projectID}>${el.name}</li>`
+    );
+  });
+  // add event listeners to the project btns
+  let projectBtns = document.querySelectorAll('.project-title');
+  projectBtns.forEach((el) => {
+    el.addEventListener('click', function () {
+      taskGroupTitle(`${el.textContent}`);
+    });
+  });
+};
 
 export { switchArrow, updateProjectList, taskGroupTitle };
